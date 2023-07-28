502e29c357820b2b6ac4634cfdedc2cf2ff2a5be
Wenlei Dai <134915780+Wenlei-Metaplay@users.noreply.github.com>
2023-07-28 18:35:53 +0300

    Leaving laptop at office pushing newest to github.

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,23 +1,59 @@
 /**
- * Takes in number as string and returns number
+ * Takes in numbers and separators a string and returns number
  */
 export class StringCalculator {
-    Add(numbers: string): number {
-        // Check if numbers string is empty and return 0 if so
-        if (numbers === "") {
-            return 0
+    escapeRegExp(string: string) {
+        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+    }
+
+    parseCustomSeparator(inputString: string): string | null {
+        if (inputString.startsWith('//')) {
+            // Extract separator
+            const match = inputString.match(/^\/\/(.+)\n/);
+            if (match) {
+                return this.escapeRegExp(match[1]);
+            }
+        }
+        return null;
+    }
+
+    joinSeparators(customSeparator: string | null): RegExp {
+        // List of default separators
+        let separators = ['\n', ','];
+
+        // If a custom separator exists, add it to the list
+        if (customSeparator) {
+            separators.push(customSeparator);
         }
 
-        // Split string by comma
-        const numberStrings = numbers.split(',')
+        // Join the separators into a single regular expression pattern
+        return new RegExp(separators.join('|'));
+    }
 
-        // Use reduce to calculate sum
-        const sum = numberStrings.reduce((accumulator, numStr) => {
-            const num = parseInt(numStr, 10);
-            return accumulator + num;
-        }, 0)
+    parseNumbers(inputString: string, separatorPattern: RegExp): number[] {
+        // Remove separator specification from the start of the string, if it exists
+        const numbersString = inputString.replace(/^\/\/(.+)\n/, '');
 
-        // Return the sum
-        return sum
+        // Split the numbers string into an array of strings
+        const numberStrings = numbersString.split(separatorPattern);
+
+        // Convert each string in the array to a number, and filter out non-numeric values
+        const numbers = numberStrings
+            .map((numStr) => parseInt(numStr, 10))
+            .filter((num) => !isNaN(num));
+
+        return numbers;
+    }
+
+    Add(inputString: string): number {
+        if (inputString === "") {
+            return 0;
+        }
+
+        const customSeparator = this.parseCustomSeparator(inputString);
+        const separatorPattern = this.joinSeparators(customSeparator);
+        const numbers = this.parseNumbers(inputString, separatorPattern);
+
+        return numbers.reduce((accumulator, num) => accumulator + num, 0);
     }
 }
--- test/string-calculator.test.ts
+++ test/string-calculator.test.ts
@@ -1,9 +1,9 @@
-import { describe, it, expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { StringCalculator } from '../src/string-calculator'
 
 const stringCalculator = new StringCalculator()
 
-describe('The Add() method should accept a string of 1 or 2 comma-separated numbers, and return their sum.', () => {
+describe('Step 1: Simple Calculator', () => {
     test('Empty string should return 0.', () => {
         expect(stringCalculator.Add("")).toBe(0) 
     })
@@ -14,5 +14,32 @@ describe('The Add() method should accept a string of 1 or 2 comma-separated numb
 
     test('2 comma-separated numbers should return their sum.', () => {
         expect(stringCalculator.Add("1,2")).toBe(3) 
+    })
+})
+
+describe('Step 2: Arbitrary number size', () => {
+    test('Unknown amount of numbers separated by comma should return their sum.', () => {
+        expect(stringCalculator.Add("1,2,3,4,5,6,7,8,9")).toBe(45)
+    })
+})
+
+describe('Step 3: Newline separator, recognises newlines as well as commas as separators.', () => {
+    test('The two separator types can be used interchangeably, numbers separated by them should return their sum', () => {
+        expect(stringCalculator.Add("1\n2,3")).toBe(6)
+    })
+})
+
+describe('Step 4: Custom separators', () => {
+    test('Custom separator should be included in the output when input starts with //', () => {
+        const separatorRegExp = stringCalculator.parseSeparator("//;\n1;2");  // Extracts the custom separator ';' from '[\\n,;]'
+        expect(separatorRegExp.toString()).toBe("/;/");
+    })
+
+    test('Optionally have custom separators in the beginning and numbers separated by it should their sum.', () => {
+        expect(stringCalculator.Add("//;\n1;2")).toBe(3)
+    })
+
+    test('Optionally have custom separators in the beginning and numbers separated by it should their sum.', () => {
+        expect(stringCalculator.Add("//;\n1;2,3")).toBe(6)
     })
 })
